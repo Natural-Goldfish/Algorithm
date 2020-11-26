e4ae6ad474a01a13e20a126dc8e24a1eddc1f078
Hoon <sang.hoon.0905@gmail.com>
2020-11-27 01:01:55 +0900

    PROGRAMMERS #K번째 수

---
+++ #K번째 수/#K번째 수.py
@@ -0,0 +1,8 @@
+import copy
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        temp = copy.deepcopy(array[commands[i][0] - 1 : commands[i][1]])
+        temp.sort()
+        answer.append(temp[commands[i][2] - 1])
+    return answer
